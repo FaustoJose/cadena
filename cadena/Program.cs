88191e9d9bd7105063c75be33c9627b43832520a
Fausto Jose Tineo Mejia <2016-2155@est.itsc.edu.do>
2021-08-08 21:05:43 -0400

    Agregar archivos de proyecto.

---
+++ cadena/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace cadena
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int veses = numLetra("lalaloodfk;s;jfdklsjldfljkg", ';');
+            Console.WriteLine("Esta letra aparece "+veses+" veses") ;
+        }
+        public static int numLetra(string cadena,char letra)
+        {
+            int u=0;
+            for (int i=0;i<=cadena.Length-1;i++) 
+            {
+                
+                if (letra==cadena[i])
+                {
+                    u = u + 1;
+                }
+                
+            }
+            return u;
+        }
+    }
+}
